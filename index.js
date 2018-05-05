ee5f3dd9f96a2563cafb919aa56cc40cbec94047
tonyamagar@gmail.com <tonyamagar@gmail.com>
2018-05-05 18:56:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,10 +28,10 @@ function checkCollision(rock) {
     const rockRightEdge = rockLeftEdge + 20;
     
     return(
-      (rockLeftEdge<=dodgerLeftEdge && rockRightEdge>=dodgerLeftEdge)
-      ()
-      
-      )
+      (rockLeftEdge<=dodgerLeftEdge && rockRightEdge>=dodgerLeftEdge) ||
+      (rockLeftEdge>=dodgerLeftEdge && rockRightEdge<=dodgerRightEdge) ||
+      (rockLeftEdge<=dodgerRightEdge && rockRightEdge>=dodgerRightEdge)
+      );
 
     
     
